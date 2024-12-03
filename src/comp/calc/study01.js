35de847eb60f071398db6244f51e77e6d837c14e
sp <tjsvlf0719@naver.com>
2024-12-03 14:17:49 +0900

    useState사용

--- src/comp/calc/study01.js
+++ src/comp/calc/study01.js
@@ -1,7 +1,41 @@
-export default function Study(){
-    return(
+import { useEffect, useState } from "react"
+
+export default function Study() {
+    const [num1, setNum1] = useState();
+    const [num2, setNum2] = useState();
+    const [op, setOp] = useState('1');
+    const result = () => {
+        switch (op) {
+            case '1':
+                return Number(num1) + Number(num2);
+            case '2':
+                return Number(num1) - Number(num2);
+            case '3':
+                return Number(num1) * Number(num2);
+            case '4':
+                return Number(num1) / Number(num2);
+        }
+    }
+
+
+    return (
         <div>
-            <h1>Study01</h1>
+            <h1>계산기 State</h1>
+            <input type="text" placeholder="첫번째 수" value={num1} onChange={
+                e => setNum1(e.target.value)
+            }></input>
+            <select onChange={
+                e => setOp(e.target.value)
+            }>
+                <option value='1'>+</option>
+                <option value='2'>-</option>
+                <option value='3'>*</option>
+                <option value='4'>/</option>
+            </select>
+            <input type="text" placeholder="두번째 수" value={num2} onChange={
+                e => setNum2(e.target.value)
+            }></input>
+            <h4>결과: {result()}</h4>
         </div>
     )
 }
