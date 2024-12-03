d794f0d166a4e626a4e393a16ad65b01af0a690f
sp <tjsvlf0719@naver.com>
2024-12-03 13:43:38 +0900

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Calc1 from './comp/calc/study01';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <About/>
+      <Routes>
+      <Route path={"/"} element={<Home/>}/>
+      <Route path={"/about"} element={<About />}/>
+      <Route path={"/cal1"} element={<Calc1 />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+function Home(){
+  return(
+    <div>
+      <h1>Start Home</h1>
+      <Link to="/about"> About로 이동</Link>
+      <Link to="/cal1"> Calc로 이동</Link>
+    </div>
+  )
+}
+function About(){
+  return(
+    <div style={{border: '2px solid blue'}}>
 
+      <Link to="/"> Home로 이동</Link>
+    </div>
+  )
+}
 export default App;
