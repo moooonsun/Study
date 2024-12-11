acc66f4aec0b5f85749897915df8c35d0b4d95f5
sp <tjsvlf0719@naver.com>
2024-12-11 09:18:16 +0900

    Feat: useState

--- src/comp/red/Red01.js
+++ src/comp/red/Red01.js
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 export default function Red01(){
+    const [data, setData] = useState(0);
     return(
         <div>
             <h1>Reducer 01</h1>
+            {data}
+            <input type="button" value='증가' onClick={
+                e=> setData(data+1)}/>
+            <input type="button" value='감소' onClick={
+                e => setData(data-1)}/>
         </div>
     )
 }
